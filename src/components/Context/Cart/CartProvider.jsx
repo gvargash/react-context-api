ddc3df9b06be3382239bc6b008ca00d37f9bb764
gvargash <gvargash2@gmail.com>
2019-08-18 23:03:39 -0500

    despachando acciones para actualizar el estado

--- src/components/Context/Cart/CartProvider.jsx
+++ src/components/Context/Cart/CartProvider.jsx
@@ -1,30 +1,36 @@
-import React, { useReducer } from "react";
+import React, {useReducer} from "react";
 import CartContext from "./CartContext";
-import { ADD_TO_CART, REMVOE_FROM_CART } from "./actions";
+import {ADD_TO_CART, REMOVE_FROM_CART} from "./actions";
 
 const initialState = {
-  cart: [1, 2, 3]
+  cart: []
 };
 
 // state & action
-const cartReducer = (state, { type, course }) => {
+const cartReducer = (state, {type, course}) => {
   if (type === ADD_TO_CART) {
     return {
       cart: state.cart.concat(course)
     };
   }
-  if (type === REMVOE_FROM_CART) {
+  if (type === REMOVE_FROM_CART) {
     return {
-      cart: state.cart.filter(c => c.id !== course)
+      cart: state.cart.filter(c => c !== course)
     };
   }
 
   return state;
 };
 
-const CartProvider = ({ children }) => {
+const CartProvider = ({
+  children
+}) => {
   const [state, dispatch] = useReducer(cartReducer, initialState);
-  return <CartContext.Provider value={[state, dispatch]}>{children}</CartContext.Provider>;
+  return <CartContext.Provider value = {
+    [state, dispatch]
+  } > {
+    children
+  } </CartContext.Provider>;
 };
 
 export default CartProvider;
--- src/components/Molecules/CourseCard.jsx
+++ src/components/Molecules/CourseCard.jsx
@@ -1,38 +1,60 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from "react-router-dom"
+import React, {useContext} from "react";
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+import CartContext from "../Context/Cart/CartContext";
+import {ADD_TO_CART, REMOVE_FROM_CART} from "./../Context/Cart/actions";
 
-const CourseCard = ({id, title, image, price, professor}) => (
+// que se vaya a la verga
+const CourseCard = ({id, title, image, price, professor}) => {
+  const [state, dispatch] = useContext(CartContext);
+  return (
     <article className="card">
-        <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-            <Link to={`/cursos/${id}`}>
-                <img src={image} alt={title} />
-            </Link>
+      <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+        <Link to={`/cursos/${id}`}>
+          <img src={image} alt={title} />
+        </Link>
+      </div>
+      <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+        <h3 className="center">{title}</h3>
+        <div className="s-main-center">{professor}</div>
+        <div className="s-main-center">
+          {state.cart.find(c => c === id) ?
+            <button onClick={()=> {
+              dispatch({type: REMOVE_FROM_CART, course: id});
+              }}
+              className="button--ghost-alert button-tinny"
+              >
+              Remover de carriot
+            </button>
+          :<button onClick={()=> {
+              dispatch({
+              type: ADD_TO_CART,
+              course: id
+              });
+            }}
+            className="button--ghost-alert button-tinny"
+            >
+            {`$ ${price} USD`}
+          </button>
+          }
         </div>
-        <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-            <h3 className="center">{title}</h3>
-            <div className="s-main-center">
-                { professor }
-            </div>
-            <div className="s-main-center">
-                <a className="button--ghost-alert button--tiny" href="https://ed.team">{ `$ ${price} USD`} </a>
-            </div>
-        </div>
+      </div>
     </article>
-)
+  );
+};
 
 CourseCard.propTypes = {
-    title: PropTypes.string,
-    image: PropTypes.string,
-    price: PropTypes.number,
-    profesor: PropTypes.string
-}
+  title: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.number,
+  profesor: PropTypes.string
+};
 
 CourseCard.defaultProps = {
-    title: "No se encontró título",
-    image: "http://www.ciudaddelapunta.com/sitio/fotos/ciudad/miniaturas/006.jpg",
-    price: "--",
-    profesor: ""
-}
+  title: "No se encontró título",
+  image: "http://www.ciudaddelapunta.com/sitio/fotos/ciudad/miniaturas/006.jpg",
+  price: "--",
+  profesor: ""
+};
 
-export default CourseCard
+export default CourseCard;
